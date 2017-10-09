cfbc2fd6a51a6b89bff8acc3656d4c1c19691a18
DanAronson <dan.ar27@gmail.com>
2017-10-09 12:46:53 +0300

    Greet a new user and alert others

--- server/server.js
+++ server/server.js
@@ -15,8 +15,20 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    socket.emit('newMessage',{
+        from: 'Admin',
+        text: 'Welcome to the chat app',
+        createdAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage',{
+        from: 'Admin',
+        text: 'A new user has joined the chat room',
+        createdAt: new Date().getTime()
+    });
+
     socket.on('disconnect', () => {
-        console.log('User Disconnected');
+        console.log('User Disconnected from server');
     });
 
     socket.on('createMessage', (message) => {
@@ -26,6 +38,11 @@ io.on('connection', (socket) => {
             text: message.text,
             createdAt: new Date().getTime()
         });
+        // socket.broadcast.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // });
     });
 });
 
