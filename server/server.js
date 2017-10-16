6a4fd95211cd70dc5f2ee2a3decf6cbc085e4d4b
DanAronson <dan.ar27@gmail.com>
2017-10-16 10:58:59 +0300

    Add Users class and update userList event

--- server/server.js
+++ server/server.js
@@ -5,12 +5,14 @@ const socketIO = require('socket.io');
 
 const {generateMessage, generateLocationMessage} = require('./utils/message');
 const {isRealString} = require('./utils/validation');
+const {Users} = require('./utils/users');
 const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, '../public');
 
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
+var users = new Users();
 
 app.use(express.static(publicPath));
 
@@ -18,12 +20,14 @@ io.on('connection', (socket) => {
     console.log('New user connected');
 
     socket.on('join', (params, callback) => {
-        // console.log('server got a join event');
-        if(!isRealString(params.name) || !isRealString(params.room)){
-            callback('Name and room name are required');
+        if (!isRealString(params.name) || !isRealString(params.room)) {
+            return callback('Name and room name are required');
         }
-        // console.log(params);
         socket.join(params.room);
+        users.removeUser(socket.id);
+        users.addUser(socket.id, params.name, params.room);
+
+        io.to(params.room).emit('updateUserList', users.getUserList(params.room));
         socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
         socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', `${params.name} has joined room: ${params.room}`));
         callback();
@@ -40,7 +44,11 @@ io.on('connection', (socket) => {
     });
 
     socket.on('disconnect', () => {
-        console.log('User Disconnected from server');
+        var user = users.removeUser(socket.id);
+        if(user){
+            io.to(user.room).emit('updateUserList', users.getUserList(user.room));
+            io.to(user.room).emit('newMessage', generateMessage('Admin', `${user.name} has left the room`));
+        }
     });
 });
 
---
+++ server/utils/users.test.js
@@ -0,0 +1,82 @@
+const expect = require('expect');
+const {Users} = require('./users');
+
+describe('Users class', () => {
+    var users;
+    beforeEach(() => {
+        users = new Users();
+        users.users = [
+            {
+                id: 1,
+                name: 'Dan',
+                room: 'Node Course'
+            },
+            {
+                id: 2,
+                name: 'Idan',
+                room: 'React Course'
+            },
+            {
+                id: 3,
+                name: 'Lior',
+                room: 'Node Course'
+            }
+        ]
+    });
+
+    it('Should add new user', () => {
+        var users = new Users();
+        var user = {
+            id: '123',
+            name: 'Dan',
+            room: 'The Office Fans'
+        };
+        var resUser = users.addUser(user.id, user.name, user.room);
+
+        expect(users.users).toEqual([user]);
+    })
+
+    it('Should return names for "Node Course"', () => {
+        var userList = users.getUserList('Node Course');
+
+        expect(userList).toEqual(['Dan', 'Lior']);
+    });
+    it('Should return names for "React Course"', () => {
+        var userList = users.getUserList('React Course');
+
+        expect(userList).toEqual(['Idan']);
+    });
+    it('Should remove a user', () => {
+        var userId = 1;
+        var user = users.removeUser(userId);
+
+        expect(users.users.length).toBe(2);
+        expect(user.id).toBe(userId);
+        expect(user.name).toBe('Dan');
+        expect(user.room).toBe('Node Course');
+    });
+
+    it('Should not remove a user', () => {
+        var userId = 5;
+        var user = users.removeUser(userId);
+
+        expect(users.users.length).toBe(3);
+        expect(user).toNotExist();
+    });
+
+    it('Should return a user', () => {
+        var userId = 1;
+        var user = users.getUser(userId);
+
+        expect(user.id).toBe(userId);
+        expect(user.name).toBe('Dan');
+        expect(user.room).toBe('Node Course');
+    });
+
+    it('Should not return a user', () => {
+        var userId = 5;
+        var user = users.getUser(userId);
+
+        expect(user).toNotExist();
+    });
+});
